e12cfca8c2e368aedc958d379239104bbcaeb024
Paul Wright <pcwright@uw.edu>
2018-05-08 12:36:54 -0700

    updated formatting and readability

--- news-article-nlp/libraries/NYTimesArticleRetriever.py
+++ news-article-nlp/libraries/NYTimesArticleRetriever.py
@@ -1,17 +1,14 @@
-# System dependencies
-import requests
-import datetime
+""" Module for reading in an analyzing NYTimes article data. """
 
+# System dependencies
+import datetime
+import requests
 
 # Standard
 import pandas as pd
 import numpy as np
 
 import text_processing
-
-
-""" Module for reading in an analyzing NYTimes article data.
-"""
 
 class NYTimesArticleRetriever():
     """ Class for reading and anlayzing NYTimes articles.
@@ -21,41 +18,46 @@ class NYTimesArticleRetriever():
             Args:
             None.
         """
-        
-        
+
+
         #self.max_features = max_features
         #self.min_df=min_df
 
-    def get_NYTimes_data(self, sections='all'):
+    def get_nytimes_data(self, sections='all'):
         """
             Function for retrieving the articles from the NYTimes by section.
             Args:
             sections: An array of sections from NYTimes to pull.
 
             Returns:
-            NYTimesData: A pandas dataframe with section names and all the section content as a string.
+            NYTimesData: A pandas dataframe with section names and all the section
+            content as a string.
         """
         if sections == 'all':
-            sections = ['arts', 'automobiles', 'books', 'business', 'fashion', 'food'
-                    , 'health',  'magazine', 'movies', 'national', 'nyregion', 'obituaries', 
-                    'politics', 'realestate', 'science', 'sports',  'technology', 
-                    'theater',  'travel',  'world']
-        
-        NYTimesData = []
+            sections = ['arts', 'automobiles', 'books', 'business', 'fashion', 'food',
+                        'health', 'magazine', 'movies', 'national', 'nyregion', 'obituaries',
+                        'politics', 'realestate', 'science', 'sports', 'technology',
+                        'theater', 'travel', 'world']
+
+        nytimes_data = []
         for section in sections:
-            url = ('http://api.nytimes.com/svc/topstories/v2/'+ section + '.json?api-key=65daae448b694b07a1dca7feb0322778')
-            s=requests.get(url).content
-            temp_data = pd.read_json(s)
-            df = ""
+            url = ('http://api.nytimes.com/svc/topstories/v2/'+ section +
+                   '.json?api-key=65daae448b694b07a1dca7feb0322778')
+            url_content = requests.get(url).content
+            temp_data = pd.read_json(url_content)
+            article_content = ""
+            #merge articles together
             for i in range(len(np.asarray(temp_data.results))):
-                df = df + str(np.asarray(temp_data.results)[i])
+                article_content = article_content + str(np.asarray(temp_data.results)[i])
 
-            NYTimesData.append([section,df])
+            nytimes_data.append([section, article_content])
 
-        NYTimesData = pd.DataFrame(np.asarray(NYTimesData))
-        NYTimesData.to_csv("../data/NYtimes_data_"+datetime.datetime.now().strftime("%Y%m%d")+".csv")
-        return NYTimesData
-    
+        nytimes_data = pd.DataFrame(np.asarray(nytimes_data))
+        nytimes_data.to_csv("../data/NYtimes_data_"+
+                            datetime.datetime.now().strftime("%Y%m%d")+
+                            ".csv")
+        return nytimes_data
+
     def get_section_words(self, data):
         """
             Function for identifying top topic words for each category.
@@ -65,16 +67,17 @@ class NYTimesArticleRetriever():
             Returns:
             topic_seeds: A list of lists with top topics for each of the inputted sections.
         """
-        processor = text_processing.ArticlePreprocessor(min_df = 0)
-        d = processor.get_dtm(series_of_articles = data.loc[:,1])
+        processor = text_processing.ArticlePreprocessor(min_df=0)
+        article_dtm = processor.get_dtm(series_of_articles=data.loc[:, 1])
 
         topic_seeds = []
-        d2 = d**2 / np.sum(d,axis=0)
+        dtm_normalized = article_dtm**2 / np.sum(article_dtm, axis=0)
 
-        for i in range(len(d2)):
-
-            words = (d2.loc[i, (d2.loc[i,:]>=5) &  
-                                      ((d2.loc[i,:] /  np.sum(d2,axis=0)) > .4 )].sort_values(ascending = False))
-            topic_seeds.append((list([data.loc[i,0]]) + list(words.reset_index().iloc[:,0])[0:15]))
+        for i in range(len(dtm_normalized)):
+            min_apps = (dtm_normalized.loc[i, :] >= 5)
+            min_probability = ((dtm_normalized.loc[i, :] /  np.sum(dtm_normalized, axis=0)) > .4)
+            words = (dtm_normalized.loc[i, min_apps & min_probability].sort_values(ascending=False))
+            category_name = list([data.loc[i, 0]])
+            top_words = list(words.reset_index().iloc[:, 0])[0:15]
+            topic_seeds.append((category_name + top_words))
         return topic_seeds
-    
