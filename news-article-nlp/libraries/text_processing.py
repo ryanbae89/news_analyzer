51271fc52ed30a820e490610d9d3211d58760ee6
helalm <helalm@uw.edu>
2018-05-15 19:40:01 -0700

    added transform() and get_vocab() methods to ArticleProcessor class

--- news-article-nlp/libraries/text_processing.py
+++ news-article-nlp/libraries/text_processing.py
@@ -1,11 +1,31 @@
+"""
+Module for preprocessing articles.
+
+Includes functions for cleaning/transforming articles,
+as well the ArticlePreprocessor() class that can be used to
+preprocess data and return a document-term-matrix.
+
+Example:
+    prep = ArticlePreprocessor()
+    prep.fit(data)
+    prep.transform(data)
+
+    # Last two lines can be replaced with either:
+    # prep.fit_transform(data) or prep.get_dtm()
+
+    # For any new query article:
+    prep.transform(article)
+"""
+
 # System dependencies
 import os
 import sys
 
-# Standard
+# Standard imports
 import pandas as pd
 import numpy as np
 import string
+import copy
 
 # NLTK imports
 from nltk import word_tokenize
@@ -15,15 +35,22 @@ from nltk.stem import WordNetLemmatizer
 # Scikit-Learn imports
 from sklearn.feature_extraction.text import CountVectorizer
 
-""" Module for preprocessing articles.
-"""
+# Module Constants
+EXTRA_STOPWORDS = [
+            "said",
+            "mr",
+            "like",
+            "ms",
+            "mrs"
+]
+
+
 
 class ArticlePreprocessor():
     """ Class for preprocessing articles.
     """
     def __init__(self, max_features=100000, min_df=5):
-        """ Constructor.
-
+        """
             Args:
             max_features: maximum number of words in the vocabulary (most common).
             min_df: float in range[0.0, 1.0], default=5. This will ignore terms
@@ -33,6 +60,43 @@ class ArticlePreprocessor():
         """
         self.max_features = max_features
         self.min_df=min_df
+        self.dtm = None
+
+    def fit(self, X, y=None):
+        """ Function for fitting preprocessor.
+
+            Args:
+            X: series of text (articles).
+            y: kept for compatability with sklearn.
+        """
+        self.get_dtm(X)
+        return self
+
+    def transform(self, X, y=None):
+        """ Function for transforming a set of articles
+            (or a single article) into a one-hot-encoding
+            with schema matching the fit dtm.
+
+            Args:
+            X: series of text (articles) or single piece of text (string).
+
+            Returns:
+            document-term-matrix (if X is series) or vector (if X is string)
+        """
+        # sklearn vectorizer needs an iterable list/series,
+        # so convert to list of just a single string
+        if isinstance(X, str):
+            X = [X] # Convert to list
+        result = self.vectorizer.transform(X)
+        dtm = self._post_process(result)
+        return dtm
+
+    def fit_transform(self, X, y=None):
+        """ Convenience function for fitting/transforming corpus.
+        """
+        self.fit(X)
+        dtm = self.transform(X)
+        return dtm
 
     def get_dtm(self, series_of_articles=None):
         """ Method for getting the document-term-matrix (dtm).
@@ -51,27 +115,50 @@ class ArticlePreprocessor():
                         documentation for more information.
         """
         if series_of_articles is None:
-            return self.dtm.copy()
+            if self.dtm is None:
+                raise ValueError("Preprocessor has not been fit. \
+                                    Provide series of articles.")
+            else:
+                return self.dtm.copy()
 
         vectorizer = CountVectorizer(preprocessor=transform_article,
                                      max_features=self.max_features,
                                      min_df=self.min_df)
 
         result = vectorizer.fit_transform(series_of_articles)
-
-        tdm = pd.DataFrame(result.toarray().transpose(),
-                            index = vectorizer.get_feature_names())
-
-        dtm = tdm.transpose()
-
-        self.dtm = dtm
         self.vectorizer = vectorizer
-        return dtm.copy()
+        dtm = self._post_process(result)
+        self.dtm = dtm.copy()
+
+        return dtm
+
+    def _post_process(self, vectorizer_output):
+        """ Internal function for processing result of sklearn
+            into a document-term-matrix.
+
+            Args:
+            vectorizer_output: output of sklearn's CountVectorizer.
+        """
+        tdm = pd.DataFrame(vectorizer_output.toarray().transpose(),
+                            index = self.vectorizer.get_feature_names())
+        dtm = tdm.transpose()
+        return dtm
+
+    def get_vocab(self):
+        """ Function for returning vocabulary of fit document-term-matrix.
+            Returns:
+            dictionary with (word: index) for (key: value)
+        """
+        if self.dtm is None:
+            raise ValueError("Preprocessor has not been fit. \
+                                Provide series of articles.")
+        return copy.deepcopy(self.vectorizer.vocabulary_)
+
 
 def clean_article(text):
     """ Helper function for cleaning an article.
         Converts to lowercase, removes punctuation,
-        removes stopwords anything that isn't alpabetic.
+        removes stopwords and anything that isn't alpabetic.
 
         Args:
         text: A string of text.
@@ -84,7 +171,7 @@ def clean_article(text):
     table = str.maketrans('', '', string.punctuation)
     stripped = [w.translate(table) for w in tokens]
     words = [word for word in stripped if word.isalpha()]
-    stop_words = set(stopwords.words('english'))
+    stop_words = list(set(stopwords.words('english'))) + EXTRA_STOPWORDS
     words_list = [w for w in words if not w in stop_words]
     return words_list
 
