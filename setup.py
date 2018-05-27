0f3142d9b1b4032b95c99f68db6e5ed7f6440901
Yumeng Crystal Ding <yd4wh@uw.edu>
2018-05-27 16:26:06 -0700

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
-
-# Setup module for news-articles-nlp
-
-#import os
-#from setuptools import setup, find_packages
-#PACKAGES = find_packages()
-
-opts = dict(name='news-articles-nlp',
-            maintainer='',
-            maintainer_email='',
-            description='News Article Recommendation System',
-            long_description=('news-articles-nlp'),
-            url='https://github.com/heybaebae/news-articles-nlp',
-            # download_url="DOWNLOAD_URL",
-            license='MIT',
-            # classifiers="CLASSIFIERS",
-            author='',
-            author_email='',
-            version='1',
-            packages=PACKAGES,
-            package_data={'news-articles-nlp': ['resources/*', 'tests/test_resources/*']},
-            # install_requires="REQUIRES",
-            # requires="REQUIRES"
-            )
-
-#if __name__ == '__main__':
-#    setup(**opts)
-
-
 import nltk
 nltk.download('punkt')
 nltk.download('vader_lexicon')
