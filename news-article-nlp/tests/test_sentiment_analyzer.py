76000bff5c14a945fc22daf85a6d6adf740e43fa
ceduze <ceduze@uw.edu>
2018-05-29 19:33:40 -0700

    more unit tests and style checks

--- news-article-nlp/tests/test_sentiment_analyzer.py
+++ news-article-nlp/tests/test_sentiment_analyzer.py
@@ -1,12 +1,12 @@
 """
 Unit Tests for Sentiment Analyzer
 """
+import unittest
 import sys
 sys.path.append('news-article-nlp/libraries')
-import unittest
+
 import sentiment_analyzer
-import matplotlib.pyplot as plt
-#from PIL import Image
+
 
 # Define a class in which the tests will run
 class SentimentAnalyzerTest(unittest.TestCase):
@@ -38,7 +38,11 @@ class SentimentAnalyzerTest(unittest.TestCase):
             null
         """
         my_sentiment = sentiment_analyzer.get_sentiment("I am happy.")
-        self.assertTrue((my_sentiment["Overall_Sentiment"] == 'Positive') & (my_sentiment["Positive_Sentences"] == 1) & (my_sentiment["Negative_Sentences"] == 0) & (my_sentiment["Neutral_Sentences"] == 0) & (my_sentiment["Total_Sentences"] == 1))
+        self.assertTrue((my_sentiment["Overall_Sentiment"] == 'Positive') &
+                        (my_sentiment["Positive_Sentences"] == 1) &
+                        (my_sentiment["Negative_Sentences"] == 0) &
+                        (my_sentiment["Neutral_Sentences"] == 0) &
+                        (my_sentiment["Total_Sentences"] == 1))
 
     def test_negative_only(self):
         """
@@ -51,7 +55,47 @@ class SentimentAnalyzerTest(unittest.TestCase):
             null
         """
         my_sentiment = sentiment_analyzer.get_sentiment("I am really sad.")
-        self.assertTrue((my_sentiment["Overall_Sentiment"] == 'Negative') & (my_sentiment["Positive_Sentences"] == 0) & (my_sentiment["Negative_Sentences"] == 1) & (my_sentiment["Neutral_Sentences"] == 0) & (my_sentiment["Total_Sentences"] == 1))
+        self.assertTrue((my_sentiment["Overall_Sentiment"] == 'Negative') &
+                        (my_sentiment["Positive_Sentences"] == 0) &
+                        (my_sentiment["Negative_Sentences"] == 1) &
+                        (my_sentiment["Neutral_Sentences"] == 0) &
+                        (my_sentiment["Total_Sentences"] == 1))
+
+    def test_neutral_only(self):
+        """
+        Test a neutral sentence
+
+        Args:
+            self (object): Reference to the class
+
+        Returns:
+            null
+        """
+        my_sentiment = sentiment_analyzer.get_sentiment("The sky is blue.")
+        self.assertTrue((my_sentiment["Overall_Sentiment"] == 'Neutral') &
+                        (my_sentiment["Positive_Sentences"] == 0) &
+                        (my_sentiment["Negative_Sentences"] == 0) &
+                        (my_sentiment["Neutral_Sentences"] == 1) &
+                        (my_sentiment["Total_Sentences"] == 1))
+
+    def test_mixed_sentiments(self):
+        """
+        Test a text with positive, negative and neutral sentences.
+
+        Args:
+            self (object): Reference to the class
+
+        Returns:
+            null
+        """
+        my_sentiment = sentiment_analyzer.get_sentiment(
+            "I am happy. I am sad. The sky is blue.")
+        self.assertTrue((my_sentiment["Overall_Sentiment"] == 'Neutral') &
+                        (my_sentiment["Positive_Sentences"] == 1) &
+                        (my_sentiment["Negative_Sentences"] == 1) &
+                        (my_sentiment["Neutral_Sentences"] == 1) &
+                        (my_sentiment["Total_Sentences"] == 3))
+
 
 if __name__ == '__main__':
     unittest.main()
