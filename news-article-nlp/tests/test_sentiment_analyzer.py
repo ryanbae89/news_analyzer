4563dbbbd42169ff6aca8f8f365df25ba1468c0f
Charles Duze <cduze@microsoft.com>
2018-05-29 19:09:38 -0700

    more unit tests for sentiment analyzer

--- news-article-nlp/tests/test_sentiment_analyzer.py
+++ news-article-nlp/tests/test_sentiment_analyzer.py
@@ -24,9 +24,34 @@ class SentimentAnalyzerTest(unittest.TestCase):
         Returns:
             null
         """
-        my_sentiment = sentiment_analyzer.get_sentiment("I am happy.")
+        my_sentiment = sentiment_analyzer.get_sentiment("This is a sentence.")
         self.assertTrue(my_sentiment is not None)
 
+    def test_positive_only(self):
+        """
+        Test a positive sentence
+
+        Args:
+            self (object): Reference to the class
+
+        Returns:
+            null
+        """
+        my_sentiment = sentiment_analyzer.get_sentiment("I am happy.")
+        self.assertTrue((my_sentiment["Overall_Sentiment"] == 'Positive') & (my_sentiment["Positive_Sentences"] == 1) & (my_sentiment["Negative_Sentences"] == 0) & (my_sentiment["Neutral_Sentences"] == 0) & (my_sentiment["Total_Sentences"] == 1))
+
+    def test_negative_only(self):
+        """
+        Test a negative sentence
+
+        Args:
+            self (object): Reference to the class
+
+        Returns:
+            null
+        """
+        my_sentiment = sentiment_analyzer.get_sentiment("I am really sad.")
+        self.assertTrue((my_sentiment["Overall_Sentiment"] == 'Negative') & (my_sentiment["Positive_Sentences"] == 0) & (my_sentiment["Negative_Sentences"] == 1) & (my_sentiment["Neutral_Sentences"] == 0) & (my_sentiment["Total_Sentences"] == 1))
 
 if __name__ == '__main__':
     unittest.main()
