ec1c83d1e541d95044fb6c13f36c2f84cc75a374
Paul Wright <pcwright@uw.edu>
2018-06-03 14:09:25 -0700

    updated user interface testing to include more function smoke tests

--- news_analyzer/tests/test_user_interface.py
+++ news_analyzer/tests/test_user_interface.py
@@ -25,11 +25,41 @@ class TestUserInterface(unittest.TestCase):
     def test_make_dash_table(self):
         """ Test to check getting NYtimes data.
         """
-        output = ui.make_dash_table(pd.DataFrame(np.asarray([[1,2],[3,4],[5,6]])))
+        output = ui.make_dash_table(pd.DataFrame(np.asarray([[1, 2],
+                                                             [3, 4],
+                                                             [5, 6]])))
         # check return types
         self.assertTrue(isinstance(output, list))
         # check length and shape of the return types
         self.assertTrue(len(output) == 3)
+
+    def test_recommended_articles(self):
+        """
+        test the update_recommended_articles function
+        """
+        output = ui.update_recommended_articles(4, "test query article")
+        self.assertIsNotNone(output)
+
+    def update_sentiment_information(self):
+        """
+        test the update_sentiment_information function
+        """
+        output = ui.update_sentiment_information(4, "test query article")
+        self.assertIsNotNone(output)
+
+    def update_top_topics(self):
+        """
+        test the update_top_topics function
+        """
+        output = ui.update_top_topics(4, "test query article")
+        self.assertIsNotNone(output)
+
+    def update_word_cloud_image(self):
+        """
+        test the update_word_cloud_image function
+        """
+        output = ui.update_word_cloud_image(4, "test query article")
+        self.assertIsNotNone(output)
 
 
 if __name__ == '__main__':
