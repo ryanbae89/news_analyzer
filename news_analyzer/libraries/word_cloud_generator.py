dd1dff1c273d5bc09b3a2353d0e9d4b36ee43f7d
ceduze <ceduze@uw.edu>
2018-06-04 09:46:16 -0700

    fixing pylint_flake8 errors

--- news_analyzer/libraries/word_cloud_generator.py
+++ news_analyzer/libraries/word_cloud_generator.py
@@ -5,9 +5,10 @@ This module has 1 function:
     generate_wordcloud (see more details below)
 """
 import base64
-import matplotlib as mpl 
+import matplotlib as mpl
 mpl.use('TkAgg')
-from wordcloud import WordCloud, STOPWORDS
+# pylint: disable=wrong-import-position
+from wordcloud import WordCloud, STOPWORDS # noqa
 STOP_WORDS = set(STOPWORDS)
 
 
