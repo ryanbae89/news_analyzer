55c587a9854dfe109ece0c4e657e48c5f2c1100b
ceduze <ceduze@uw.edu>
2018-06-04 14:50:10 -0700

    fixing pylint_flake8 errors 5

--- news_analyzer/libraries/text_processing.py
+++ news_analyzer/libraries/text_processing.py
@@ -40,6 +40,7 @@ EXTRA_STOPWORDS = [
     "mrs"
 ]
 
+
 def transform_article(article):
     """ Function for transforming a single article.
         Cleans text (see clean_article function) and performs
@@ -56,6 +57,7 @@ def transform_article(article):
     lemmatized = [lemmatizer.lemmatize(token) for token in tokens]
     transformed_article = " ".join(lemmatized)
     return transformed_article
+
 
 def clean_article(text):
     """ Helper function for cleaning an article.
@@ -77,17 +79,20 @@ def clean_article(text):
     words_list = [w for w in words if not w in stop_words]
     return words_list
 
+
 class ArticlePreprocessor():
     """ Class for preprocessing articles.
     """
     def __init__(self, max_features=100000, min_df=5):
         """
             Args:
-            max_features: maximum number of words in the vocabulary (most common).
-            min_df: float in range[0.0, 1.0], default=5. This will ignore terms
-                    that have a document frequency strictly lower than the given
-                    threshold. If float, the parameter represents a proportion of
-                    documents. If integer, the absolute counts.
+            max_features: maximum number of words in the vocabulary
+                    (most common).
+            min_df: float in range[0.0, 1.0], default=5. This will ignore
+                    terms that have a document frequency strictly lower
+                    than the given threshold. If float, the parameter
+                    represents a proportion of documents. If integer,
+                    the absolute counts.
         """
         self.max_features = max_features
         self.min_df = min_df
@@ -98,36 +103,39 @@ class ArticlePreprocessor():
         """ Function for fitting preprocessor.
 
             Args:
-            series of articles: pandas series of text (articles) or a single text.
+            series of articles: pandas series of text (articles) or a
+                    single text.
 
             Returns:
             self
 
             Raises:
-            ValueError() if not enough 'clean' words (as determined by clean_article)
-            to fit model.
+            ValueError() if not enough 'clean' words (as determined by
+                    clean_article) to fit model.
         """
         # Checks:
         # Convert to list if string (required by sklearn CountVectorizer)
         if isinstance(series_of_articles, str):
-            series_of_articles = [series_of_articles] # Convert to list
+            series_of_articles = [series_of_articles]  # Convert to list
 
-        # Ensure the transformation will return at least 1 word. Otherwise raise
-        # ValueError.
+        # Ensure the transformation will return at least 1 word. Otherwise
+        # raise ValueError.
         cleaned_words = []
         for article in series_of_articles:
             cleaned_words = cleaned_words + transform_article(article).split()
         cleaned_words = list(set(cleaned_words))
-        num_words = len(cleaned_words) # NOTE: This isn't the full vocabulary,
-                                       # - just a good approximation (non-single-letters)
+        # NOTE: This isn't the full vocabulary,
+        # - just a good approximation (non-single-letters)
+        num_words = len(cleaned_words)
 
         if num_words == 0:
             raise ValueError("Article(s) do not contain any \
                                 alphabetic words to parse.")
 
-        # Make sure the min_df (min. times of usage for a word) isn't too strict.
-        # If number of words isn't 5 times min_df the min_df will be set to zero.
-        # This will guarantee a dtm of with number of columns equal to min(5, num_words).
+        # Make sure the min_df (min. times of usage for a word) isn't
+        # too strict. If number of words isn't 5 times min_df the min_df
+        # will be set to zero. This will guarantee a dtm of with number
+        # of columns equal to min(5, num_words).
         if num_words < 5*self.min_df:
             self.min_df = 0
 
@@ -150,7 +158,7 @@ class ArticlePreprocessor():
         # sklearn vectorizer needs an iterable list/series,
         # so convert to list of just a single string
         if isinstance(series_of_articles, str):
-            series_of_articles = [series_of_articles] # Convert to list
+            series_of_articles = [series_of_articles]  # Convert to list
         result = self.vectorizer.transform(series_of_articles)
         dtm = self._post_process(result)
         return dtm
--- news_analyzer/scripts/build_resources.py
+++ news_analyzer/scripts/build_resources.py
@@ -17,14 +17,14 @@ import pandas as pd
 import numpy as np
 
 sys.path.append("../libraries")
-import configs
-import text_processing
-import topic_modeling
+import configs # noqa
+import text_processing # noqa
+import topic_modeling # noqa
 
-## Module Constants
+# Module Constants
 KAGGLE_COMP = "all-the-news"
 CSV_NAMES = ["articles1.csv", "articles2.csv", "articles3.csv"]
-#CSV_NAMES = ["articles.csv"]
+# CSV_NAMES = ["articles.csv"]
 RESOURCE_PATH = "../" + configs.RESOURCE_FOLDER
 FPATHS = [RESOURCE_PATH + "/" + name for name in CSV_NAMES]
 CONTENT_COLUMN = "content"
@@ -32,6 +32,7 @@ MIN_WORDS_IN_ARTICLE = 200
 BAD_GUIDED_TOPICS = ['national', 'nyregion', 'obituaries']
 GUIDED_TOPICS_CONFIDENCE = 0.5
 N_ITERATIONS = 100
+
 
 def get_files():
     """ Function for downloading Kaggle files (see CSV_NAMES in module header),
@@ -47,10 +48,12 @@ def get_files():
         list_of_tables.append(pd.read_csv(fpath, encoding='utf8'))
 
     full_table = pd.concat(list_of_tables)
-    article_lengths = full_table[CONTENT_COLUMN].apply(lambda x: len(x.split()))
+    article_lengths = \
+        full_table[CONTENT_COLUMN].apply(lambda x: len(x.split()))
     full_table = full_table[article_lengths > MIN_WORDS_IN_ARTICLE]
     full_table.to_csv(configs.CORPUS_PATH, index=False)
     return full_table
+
 
 if __name__ == "__main__":
     """ Main script used to download Kaggle files, preprocessing data,
@@ -58,13 +61,16 @@ if __name__ == "__main__":
         folder as csv files (data) or .pkl (objects).
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--download_files', dest='download_files', action='store_true', required=False)
+    parser.add_argument('--download_files',
+                        dest='download_files',
+                        action='store_true',
+                        required=False)
     parser.set_defaults(download_files=False)
     args = parser.parse_args()
     download_files = args.download_files
 
     # If corpus csv does not exist download and build.
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     if not os.path.isfile(configs.CORPUS_PATH) or download_files:
         full_table = get_files()
     else:
@@ -79,22 +85,36 @@ if __name__ == "__main__":
     vocab, word2id = topic_modeling.get_vocab(dtm)
 
     # Get nyt seed topics
-    topics_raw = nytimes_article_retriever.get_nytimes_topic_words() # to be changed
-    topics_clean = topic_modeling.clean_topics(topics_raw, vocab, word2id, BAD_GUIDED_TOPICS)
-    seed_topics = topic_modeling.get_seed_topics(topics_clean, word2id)
+    topics_raw = \
+        nytimes_article_retriever.get_nytimes_topic_words()  # to be changed
+    topics_clean = topic_modeling.clean_topics(topics_raw,
+                                               vocab,
+                                               word2id,
+                                               BAD_GUIDED_TOPICS)
+    seed_topics = topic_modeling.get_seed_topics(topics_clean,
+                                                 word2id)
 
     # Fit guided LDA model
     n_guided_topics = len(topics_clean)
-    guidedlda_model = topic_modeling.TopicModeler(n_guided_topics, N_ITERATIONS, RANDOM_STATE, REFRESH)
-    guidedlda_model = guidedlda_model.fit(dtm, seed_topics, GUIDED_TOPICS_CONFIDENCE)
+    guidedlda_model = topic_modeling.TopicModeler(n_guided_topics,
+                                                  N_ITERATIONS,
+                                                  RANDOM_STATE,
+                                                  REFRESH)
+    guidedlda_model = guidedlda_model.fit(dtm,
+                                          seed_topics,
+                                          GUIDED_TOPICS_CONFIDENCE)
 
     # Fit unguided LDA model
     n_unguided_topics = len(dtm)/100
-    unguidedlda_model = topic_modeling.TopicModeler(n_unguided_topics, N_ITERATIONS, RANDOM_STATE, REFRESH)
+    unguidedlda_model = topic_modeling.TopicModeler(n_unguided_topics,
+                                                    N_ITERATIONS,
+                                                    RANDOM_STATE,
+                                                    REFRESH)
     unguidedlda_model = unguidedlda_model.fit(dtm)
 
     # Save results
-    unguidedlda_model.purge_extra_matrices() # unguided model is large, so get rid of extra matrices
+    # unguided model is large, so get rid of extra matrices
+    unguidedlda_model.purge_extra_matrices()
     with open(RESOURCE_PATH + 'guidedlda_model.pkl', 'wb') as file_handle:
         pickle.dump(guidedlda_model, file_handle)
     with open(RESOURCE_PATH + 'unguidedlda_model.pkl', 'wb') as file_handle:
