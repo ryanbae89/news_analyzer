1f4103d9a3cbf7b5022ff31995b7541e29a27f35
Yumeng Crystal Ding <yd4wh@uw.edu>
2018-06-05 11:58:45 -0700

    updated unittest for recommender

--- news_analyzer/tests/test_article_recommender.py
+++ news_analyzer/tests/test_article_recommender.py
@@ -9,37 +9,54 @@ Functions:
     returns 5 indexes to join to corpus
     test_knn_prediction: A function that checks KDTree
     returns nearest index based on distance
+    test_join_type: A function that checks the join_process return type
 
 """
 # standard imports
 import sys
 import unittest
 import numpy as np
+import pandas as pd
 
 # pylint: disable=wrong-import-position
 sys.path.append('news_analyzer/libraries') # noqa
+import configs
 import article_recommender
 
 
 class TestArticleRecommender(unittest.TestCase):
     """A Class of functions to perform unittest on article_recommender"""
+    def setUp(self):
+        """
+        Initializer for TestArticleRecommender class.
+        This function loads the corpus for testing.
+        """
+        self.corpus = pd.read_csv(configs.CORPUS_PATH)
+        self.knn_dimension = article_recommender.knn_prediction(
+            np.zeros(shape=(5, 10)), [np.zeros(10)])
+        self.knn_logic = article_recommender.knn_prediction(
+            np.repeat(np.array([(range(10))]), 10, axis=0).T, [(np.zeros(10))])
+        self.join_index = article_recommender.join_process(
+            np.array([0, 1, 2]), self.corpus)
+
     def test_knn_dimension(self):
         """This function checks whether knn_prediction returns 5 nearest indexes
         """
         # Pass in matrix of all zeros for doc_topic_matrix
         # Check knn_prediction returns 5 nearest relevance topics
-        self.assertTrue(article_recommender.
-                        knn_prediction(np.zeros(shape=(5, 10)),
-                                       [np.zeros(10)])[1].shape[1] == 5)
+        self.assertTrue(self.knn_dimension[1].shape[1] == 5)
 
     def test_knn_prediction(self):
         """This function checks whether knn_prediction returns correct nearest indexes
         """
         # Pass in matrix of 0 through 9 for doc_topic_matrix
         # Check all-zero vector is closest to the first row of doc_topic_matrix
-        self.assertTrue((article_recommender.knn_prediction(
-            np.repeat(np.array([(range(10))]), 10, axis=0).T,
-            [(np.zeros(10))])[1] == [[0, 1, 2, 3, 4]]).all)
+        self.assertTrue((self.knn_logic[1] == [0, 1, 2, 3, 4]).all)
+
+    def test_join_type(self):
+        """This function check the type of the join process
+        """
+        self.assertTrue(isinstance(self.join_index, str))
 
 
 if __name__ == '__main__':
